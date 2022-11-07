ea389b4dfe9de37ef8d7f3a0c1d72e1beca7238d
rafa <rafumojica1@gmail.com>
2022-11-07 15:14:16 +0800

    fixed drops displaying on empty array

--- src/pages/Item.jsx
+++ src/pages/Item.jsx
@@ -91,11 +91,11 @@ function Item() {
                 Cooking Effect: {capitalizeName(item.cooking_effect)}
               </h3>
             )}
-            {!!item.drops ? (
+            {item.drops === [] ? null : item.drops.length > 0 ? (
               <h3 className="text-sm text-center mt-2 md:text-base lg:text-lg">
                 Drops: {item.drops.join(", ")}
               </h3>
-            ) : item.drops === [] ? null : null}
+            ) : null}
             <Stats item={item.attack} itemName="Attack" />
             <Stats item={item.defense} itemName="Defense" />
           </div>
